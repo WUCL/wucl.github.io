415c7bd765af52e488a6b002c10c7a0a0f7df449
WUCL <allenwu8098@gmail.com>
2018-03-04 23:24:41 +0800

    小樣式更新

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-console.log('%cHi! My name is Chung-Lun Wu, You can call me Allen.', 'color:#dc4776;padding-left:9px;font-weight:bolder;font-size:50px;');
+console.log('%cHi! This is Chung-Lun Wu.', 'color:#dc4776;padding-left:9px;font-weight:bolder;font-size:21px;');
 // $('._home__resume, ._exp__mmc').on('click', '._content ._part h3', function(){
 // 	$(this).closest('._part').toggleClass('_show');
 // })
