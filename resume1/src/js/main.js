88ee9173bfd46ec575cb1310deffc394fac3ba42
allenwu <allenwu@pixnet.tw>
2020-06-09 11:06:20 +0800

    更新一版 resum1

---
+++ resume1/src/js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+    var MAIN = {
+        env: 'html',
+        el: {
+            $window: $(window),
+            $doc: $(document),
+            $body: $('body'),
+            $header: $('#header'),
+            $main: $('#main'),
+            $footer: $('#footer'),
+        },
+        init: function() {
+            if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
+            this.el.$body.addClass(deviceObj.name);
+            this.bindEvent();
+        },
+        bindEvent: function() {
+            let $this = this;
+        },
+        doAos: function() {
+            return AOS.init({
+                duration: 300,
+                offset: 150,
+                delay: 0,
+                once: true,
+                easing: 'ease-in'
+            });
+        },
+        doScrollIt: function() {
+            let $this = this;
+            $.scrollIt({
+                upKey: 38,             // key code to navigate to the next section
+                downKey: 40,           // key code to navigate to the previous section
+                easing: 'easeInOutExpo',      // the easing function for animation
+                scrollTime: 600,       // how long (in ms) the animation takes
+                activeClass: 'active', // class given to the active nav element
+                onPageChange: function(e) {},    // function(pageIndex) that is called when page is changed
+                topOffset: -130           // offste (in px) for fixed top navigation
+            });
+        },
+    };
+    MAIN.init();
+});
---
+++ resume1/src/js/script.js
@@ -0,0 +1,7 @@
+console.log('%cHi! This is Chung-Lun Wu.', 'color:#dc4776;padding-left:9px;font-weight:bolder;font-size:21px;');
+// $('._home__resume, ._exp__mmc').on('click', '._content ._part h3', function(){
+// 	$(this).closest('._part').toggleClass('_show');
+// })
+// $('._exp__mmc').on('click', '._controller', function(){
+// 	$(this).closest('.__left').toggleClass('_play');
+// })
