a5684938b782c5b0a406f1b29a6f5f691b79ae03
allenwu <allenwu@pixnet.tw>
2020-09-27 18:30:34 +0800

    重新更新一版
    調整樣式文案

--- resume2020/public/js/main.js
+++ resume2020/public/js/main.js
@@ -40,53 +40,6 @@
             externalLink(url) {
                 window.open(url, '_blank');
             },
-            openExp(e) {
-                let $target = e.currentTarget;
-                let $exp = $target.getAttribute('data-exp');
-                $target.classList.add("active");
-                window.helper.updateUrlParams(location.href, 'exps', $exp, true);
-
-                let $exps = this.exps[$exp];
-
-                let $exps_el = document.getElementById("exps");
-                let $bottom_el = $exps_el.querySelector(".container .bottom");
-                $exps_el.querySelector('.logo img').src = $exps.logo;
-                $exps_el.querySelector('.info time').innerHTML = $exps.time;
-                $exps_el.querySelector('.info .name').href = $exps.url;
-                $exps_el.querySelector('.info .name').innerHTML = $exps.name;
-                $exps_el.querySelector('.info .title').innerHTML = $exps.title;
-                $exps_el.setAttribute("data-exp", $exp);
-
-                let $lists = $exps.lists;
-                $lists.forEach(lists => {
-                    let $title = lists.title;
-                    let $item = lists.item;
-                    let $temp_items = '';
-                    let $list_el = document.createElement("div");
-                    let $ul_el = document.createElement("ul");
-                    $list_el.setAttribute("data-title", lists.title);
-                    $item.forEach(item => $temp_items += '<li>' + item + '</li>');
-                    $ul_el.innerHTML = $temp_items;
-                    $list_el.append($ul_el);
-                    $bottom_el.append($list_el);
-                });
-
-                // this.el.$body.addClass('disableScroll').addClass('open-exps');
-                this.el.$body.classList.add('disableScroll');
-                this.el.$body.classList.add('open-exps');
-            },
-            closeExp() {
-                let $target = document.querySelector("#about-exp ul li.active");
-                $target.classList.remove("active");
-                let $exps_el = document.getElementById("exps");
-                let $bottom_el = $exps_el.querySelector(".container .bottom");
-                $exps_el.setAttribute('data-exp', '');
-                $bottom_el.innerHTML = "";
-                window.helper.updateUrlParams(location.href, 'exps', '', true);
-                // this.el.$body.removeClass('disableScroll').removeClass('open-exps');
-                this.el.$body.classList.remove('disableScroll');
-                this.el.$body.classList.remove('open-exps');
-            },
         }
     });
 
