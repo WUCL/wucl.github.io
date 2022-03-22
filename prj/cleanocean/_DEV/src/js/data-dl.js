aa4d9147e282ec7455eb21e37c5fdb08622336e4
ALLENWU <allenwu8098@gmail.com>
2022-03-23 02:02:26 +0800

    0323更新進度
    **TODO**
    
    [index.html]
    1. 缺點擊地圖效果
    
    [data.html]
    1. 缺點擊地圖效果（同首頁）
    
    [album.html]
    1. 點開相簿
    
    [member.html]
    1. 編輯會員（overlay）
    2. 上傳大頭照功能，同上
    3. 我的淨灘足跡，要可以新增刪除
    4. 成果圖產生器（合成圖壓文）（需算計圖片XY軸）
    
    [postcard-builder.html]
    1. 上傳照片（大小限制）
    2. 字數判斷（右側填寫區）
    3. 明信片產生器（合成圖壓文）（需算計圖片XY軸）（文字字數分段）
    4. 還要可以從相簿選圖
    
    **DONE**
    [info.html]
    [data-dl.html]

--- prj/cleanocean/_DEV/src/js/data-dl.js
+++ prj/cleanocean/_DEV/src/js/data-dl.js
@@ -7,15 +7,138 @@ $(function() {
             $header: $('#header'),
             $main: $('#main'),
             $footer: $('#footer'),
+
+            $filterTimeBegin: $('#filter-time-begin'),
+            $filterTimeEnd: $('#filter-time-end'),
+            $filterCampaign: $('#filter-campaign'),
+
+            $selectAll: $('#select-all'),
+            $filterResultList: $('#filter-result-list'),
         },
         var: {
+            $api: {
+
+            },
+            $filter: {
+                timeBegin: '',
+                timeEnd: '',
+                county: '',
+                campaign: ''
+            },
+            $dl: [],
         },
         init: function() {
             console.log('data-dl');
             this.bindEvent();
+            this.goInitial(); // 先 ajax 拿到資料先builder
+            this.listenFilter();
         },
         bindEvent: function() {
             let $this = this;
+            let $filterVal = $this.var.$filter;
+            let $dl = $this.var.$dl;
+            $('#filter-search').on('click', function() {
+                if (($filterVal.timeBegin.length + $filterVal.timeEnd.length + $filterVal.county.length + $filterVal.campaign.length) > 0) {
+                    $this.goFilter();
+                } else {
+                    return alert('請設定搜尋內容');
+                }
+            });
+            $('#filter-download').on('click', function() {
+                console.log('download');
+                if ($this.var.$dl.length > 0) {
+                    $this.goDownload();
+                } else {
+                    return alert('請勾選欲下載數據');
+                }
+            });
+        },
+        goInitial: function() {
+            let $this = this;
+            console.log('goInitial');
+            // $this.buildResult(window.dataDl);
+        },
+        buildResult: function(list) {
+            let $list = list;
+            console.log('buildResult');
+            console.log($list);
+        },
+        listenFilter: function() {
+            console.log("listenFilter");
+            let $this = this;
+            $('#twzipcode').twzipcode({
+                onCountySelect: function(e) {
+                    $this.var.$filter.county = $('#twzipcode').twzipcode('get', 'county')[0];
+                    console.log($this.var.$filter);
+                },
+            });
+            $this.el.$filterTimeBegin.on("change", function(e) {
+                $this.var.$filter.timeBegin = e.target.value;
+                $this.el.$filterTimeEnd.attr("min", $this.var.$filter.timeBegin);
+                console.log($this.var.$filter);
+            });
+            $this.el.$filterTimeEnd.on("change", function(e) {
+                $this.var.$filter.timeEnd = e.target.value;
+                $this.el.$filterTimeBegin.attr("max", $this.var.$filter.timeEnd);
+                console.log($this.var.$filter);
+            });
+            $this.el.$filterCampaign.on("blur", function(e) {
+                $this.var.$filter.campaign = e.target.value;
+                console.log($this.var.$filter);
+            });
+            $this.el.$filterResultList.on("change", "input[type=checkbox]", function(e) {
+                console.log(e);
+                $this.updateDLCheckedList();
+            });
+            $this.el.$selectAll.on("change", function(e) {
+                $this.el.$filterResultList.find('input[type=checkbox]').prop('checked', e.target.checked);
+                $this.updateDLCheckedList();
+            });
+        },
+        updateDLCheckedList: function() {
+            console.log('updateDLCheckedList');
+            let $this = this;
+            $this.var.$dl = [];
+            $this.el.$filterResultList.find('input[type=checkbox]:checked').each(function () {
+                $this.var.$dl.push(this.value);
+            });
+            console.log($this.var.$dl);
+            return;
+        },
+        goFilter: function() {
+            console.log("goFilter");
+            let $this = this;
+            console.log($this.var.$filter);
+
+            // // ajax url
+            // let _url = $this.var.api + '&__r=' + (new Date()).getTime();
+            // // ajax handle
+            // $.ajax({
+            //     url: _url,
+            //     type: 'get',
+            //     dataType: 'json',
+            //     success: (response) => {
+            //         console.log(response);
+            //         if (response.error === 0) {
+            //             doSuccess(response.counts);
+            //         }
+            //         return;
+            //     },
+            //     error: function(response) {
+            //         console.log('error');
+            //         console.log(response);
+            //     }
+            // });
+            doSuccess(window.dataDl);
+            function doSuccess(response) {
+                let $response = response;
+                console.log($response);
+
+                return $this.buildResult($response);
+            };
+        },
+        goDownload: function() {
+            console.log("goDownload");
         },
     };
     DATA.init();
--- prj/cleanocean/public/js/album.js
+++ prj/cleanocean/public/js/album.js
@@ -7,15 +7,106 @@ $(function() {
             $header: $('#header'),
             $main: $('#main'),
             $footer: $('#footer'),
+
+            $filterTimeBegin: $('#filter-time-begin'),
+            $filterTimeEnd: $('#filter-time-end'),
+            $filterCampaign: $('#filter-campaign'),
+
+            $filterResultList: $('#filter-result-list'),
         },
         var: {
+            $api: {
+
+            },
+            $filter: {
+                timeBegin: '',
+                timeEnd: '',
+                county: '',
+                campaign: ''
+            },
         },
         init: function() {
             console.log('album');
             this.bindEvent();
+            this.goInitial(); // 先 ajax 拿到資料先builder
+            this.listenFilter();
         },
         bindEvent: function() {
             let $this = this;
+            let $filterVal = $this.var.$filter;
+            $('#filter-search').on('click', function() {
+                if (($filterVal.timeBegin.length + $filterVal.timeEnd.length + $filterVal.county.length + $filterVal.campaign.length) > 0) {
+                    $this.goFilter();
+                } else {
+                    return alert('請設定搜尋內容');
+                }
+            });
+        },
+        goInitial: function() {
+            let $this = this;
+            console.log('goInitial');
+            // $this.buildAlbum(window.album);
+        },
+        buildAlbum: function(list) {
+            let $list = list;
+            console.log('buildAlbum');
+            console.log($list);
+        },
+        listenFilter: function() {
+            console.log("listenFilter");
+            let $this = this;
+            $('#twzipcode').twzipcode({
+                onCountySelect: function(e) {
+                    $this.var.$filter.county = $('#twzipcode').twzipcode('get', 'county')[0];
+                    console.log($this.var.$filter);
+                },
+            });
+            $this.el.$filterTimeBegin.on("change", function(e) {
+                $this.var.$filter.timeBegin = e.target.value;
+                $this.el.$filterTimeEnd.attr("min", $this.var.$filter.timeBegin);
+                console.log($this.var.$filter);
+            });
+            $this.el.$filterTimeEnd.on("change", function(e) {
+                $this.var.$filter.timeEnd = e.target.value;
+                $this.el.$filterTimeBegin.attr("max", $this.var.$filter.timeEnd);
+                console.log($this.var.$filter);
+            });
+            $this.el.$filterCampaign.on("blur", function(e) {
+                $this.var.$filter.campaign = e.target.value;
+                console.log($this.var.$filter);
+            });
+        },
+        goFilter: function() {
+            console.log("goFilter");
+            let $this = this;
+            console.log($this.var.$filter);
+
+            // // ajax url
+            // let _url = $this.var.api + '&__r=' + (new Date()).getTime();
+            // // ajax handle
+            // $.ajax({
+            //     url: _url,
+            //     type: 'get',
+            //     dataType: 'json',
+            //     success: (response) => {
+            //         console.log(response);
+            //         if (response.error === 0) {
+            //             doSuccess(response.counts);
+            //         }
+            //         return;
+            //     },
+            //     error: function(response) {
+            //         console.log('error');
+            //         console.log(response);
+            //     }
+            // });
+            doSuccess(window.dataDl);
+            function doSuccess(response) {
+                let $response = response;
+                console.log($response);
+
+                return $this.buildAlbum($response);
+            };
         },
     };
     ALBUM.init();
