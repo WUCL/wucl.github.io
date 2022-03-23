c4749a23b4b8095342f5b58bce8bc69020db6d28
ALLENWU <allenwu8098@gmail.com>
2022-03-24 02:06:37 +0800

    member name popupup

--- prj/cleanocean/_DEV/src/js/member.js
+++ prj/cleanocean/_DEV/src/js/member.js
@@ -9,13 +9,45 @@ $(function() {
             $footer: $('#footer'),
         },
         var: {
+            $m: {
+                name: '',
+                avatar: '',
+            },
         },
         init: function() {
             console.log('member');
             this.bindEvent();
+            this.setPopup();
         },
         bindEvent: function() {
             let $this = this;
+            $('#m-editor-avatar').on('change', function() {
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var output = document.getElementById('preview_avatar');
+                    output.src = reader.result;
+                }
+                reader.readAsDataURL(event.target.files[0]);
+            });
+            $('#btn-m-editor-update').on('click', function() {
+                $this.var.$m.avatar = $('#preview_avatar').attr('src');
+                $this.var.$m.name = $('#m-editor-name').val();
+                console.log($this.var.$m);
+                if ($this.var.$m.avatar != '') {
+                    $('#m-avatar').attr('src', $this.var.$m.avatar);
+                }
+                if ($this.var.$m.name != $('#m-name').val()) {
+                    $('#m-name').val($this.var.$m.name);
+                }
+                $('#edit-member').popup('hide');
+            });
+        },
+        setPopup: function() {
+            $('#edit-member').popup({
+                escape: false,
+                closebutton: true,
+                scrolllock: true,
+            });
         },
     };
     MEMBER.init();
