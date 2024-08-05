f879a7b8bb679cecb3f029d9bc68cfb7df87a065
chunglunwu <allenwu8098@gmail.com>
2024-08-05 18:05:23 +0800

    上線後更新
    1. Mobile KV 調整
    2. 加入滑動進場特效

--- prj/202408-BeamSuntory_auchentoshan/_DEV/src/js/main.js
+++ prj/202408-BeamSuntory_auchentoshan/_DEV/src/js/main.js
@@ -14,6 +14,7 @@ $(function() {
             if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
             this.el.$body.addClass(deviceObj.name);
             this.bindEvent();
+            this.doAos();
         },
         bindEvent: function() {
             let $this = this;
@@ -24,6 +25,15 @@ $(function() {
                 let $folder_switch = $folder.attr('data-folder');
                 if ($folder_switch == 1) $folder.attr('data-folder', 0);
                 else $folder.attr('data-folder', 1);
+            });
+        },
+        doAos: function() {
+            return AOS.init({
+                duration: 650,
+                offset: 300,
+                delay: 0,
+                once: true,
+                easing: 'ease-in-out'
             });
         },
     };
