02a7907be8bbc0b7ebf445a5d2a26f2f9db02ec3
chunglunwu <allenwu8098@gmail.com>
2024-10-02 01:40:21 +0800

    更新搜尋欄位手機格式
    1. 搜尋欄位手機格式
    2. 處理判斷值

--- prj/202409-BeamSuntory_Hibiki/_DEV/src/js/main.js
+++ prj/202409-BeamSuntory_Hibiki/_DEV/src/js/main.js
@@ -10,6 +10,8 @@ $(function() {
             $footer: $('#footer'),
 
             $_searcher: $('#_searcher'),
+            $searchbar: $('#searchbar'),
+            $ticket: $('#ticket'),
 
             $s_it: $('#s_it'),
             $btn_submit: $('#btn-submit'),
@@ -18,6 +20,7 @@ $(function() {
             $carouselit: $('#carouselit'),
         },
         var: {
+            _phone: '',
         },
         init: function() {
             if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
@@ -29,15 +32,33 @@ $(function() {
         bindEvent: function() {
             let $this = this;
 
-            $this.el.$s_it.on('keyup', (e) => {
-                console.log(e.target.value);
+            $this.el.$s_it.on('keypress', (e) => {
                 console.log('$s_it');
-                if (!/^\d*$/.test(e.target.value || '')) return;
+
+                // Only ASCII character in that range allowed
+                let ASCIICode = (e.which) ? e.which : e.keyCode
+                if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+                    return false;
+                return true;
             });
             $this.el.$btn_submit.on('click', (e) => {
                 console.log('$s_submit');
 
-                $this.el.$_searcher.attr('data-layer', 2);
+                const regex = /^09[0-9]{8}$/;
+                let _s_it = $this.el.$s_it;
+                let _phone = _s_it.val();
+                let _vaild = regex.test(_phone);
+                // console.log(_phone);
+                // console.log(_vaild);
+
+                if (!_vaild) {
+                    $this.el.$searchbar.addClass('_error');
+                } else {
+                    $this.el.$searchbar.removeClass('_error');
+                    $this.var._phone = _phone; // if the number format correct
+
+                    $this.doTicket(); // call ticket number
+                }
                 return;
             });
 
@@ -45,9 +66,30 @@ $(function() {
             $this.el.$btn_research.on('click', (e) => {
                 console.log('research');
 
-                $this.el.$_searcher.attr('data-layer', 1);
+                // claer and empty
+                $this.var._phone = '';
+                $this.el.$s_it.val(''); // empty search bar
+
+                $this.el.$_searcher.attr('data-layer', 1); // change layer back to 'search'
                 return;
             });
+        },
+
+        doTicket: function() {
+            let $this = this;
+
+            console.log('doTicket');
+            console.log($this.var._phone);
+
+            let _ticket;
+            //
+
+            if ($this.var._phone == '0912345678') return alert('查無此號碼'); // just for test, need to change
+
+            _ticket = '20398';
+            //
+            $this.el.$_searcher.attr('data-layer', 2); // change layer to 'ticket'
+            return $this.el.$ticket.attr('data-num', _ticket);
         },
 
         carouselit: function() {
