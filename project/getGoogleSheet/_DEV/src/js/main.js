4e1433c8599da020b8a268fa22886ca3b15ee992
chunglunwu <allenwu8098@gmail.com>
2025-10-16 12:50:56 +0800

    測試 get google sheet

---
+++ project/getGoogleSheet/_DEV/src/js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    var MAIN = {
+        env: 'html',
+        el: {
+            $window: $(window),
+            $doc: $(document),
+            $body: $('body'),
+            $header: $('#header'),
+            $main: $('#main'),
+            $footer: $('#footer'),
+            $nav: $('#nav'),
+            $navSwitch: $('#switch'),
+        },
+        var: {
+        },
+        init: function() {
+            if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; }
+            this.el.$body.addClass(deviceObj.name);
+            this.bindEvent();
+        },
+        bindEvent: function() {
+            let $this = this;
+
+            $this.el.$navSwitch.on('click', function() {
+                $this.el.$body.toggleClass('openheader');
+            });
+        },
+    };
+    MAIN.init();
+});
